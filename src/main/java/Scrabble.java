efb494794946f86b902875318fc0e97bbd9b8e6d
jocelyn & nataliya <student@epicodus.com>
2015-08-11 12:09:50 -0700

    Added Velocity Templates Added Home Route

--- src/main/java/Scrabble.java
+++ src/main/java/Scrabble.java
@@ -2,10 +2,36 @@ import java.io.Console;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Scrabble {
-    public static void main(String[] args){}
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
 
-    public Integer getScore(String userWord) {
+public class Scrabble {
+    public static void main(String[] args){
+        String layout = "templates/layout.vtl";
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "templates/home.vtl");
+            return new ModelAndView(model, layout);
+
+        }, new VelocityTemplateEngine());
+
+        get("/detector", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "templates/detector.vtl");
+
+            String word = request.queryParams("word");
+            Integer getScore = getScore(word);
+
+            return new ModelAndView(model, layout);
+
+        }, new VelocityTemplateEngine());
+
+
+    }
+
+    public static Integer getScore(String userWord) {
         // Integer x = 0;
         String word = userWord.toLowerCase();
         Integer total = 0;
