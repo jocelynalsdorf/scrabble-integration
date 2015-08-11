e56125d6e2e55862ce29834c97f0fb24e23bc285
jocelyn & nataliya <student@epicodus.com>
2015-08-11 12:16:05 -0700

    Tests are still passing with astatic Scrabble method

--- src/test/java/ScrabbleTest.java
+++ src/test/java/ScrabbleTest.java
@@ -5,29 +5,25 @@ public class ScrabbleTest {
 
     @Test
     public void scrabbleScore_returnsAscrabbleScoreForALetter_1() {
-        Scrabble testScrabble = new Scrabble();
         Integer score = 2;
-        assertEquals(score, testScrabble.getScore("g"));
+        assertEquals(score, Scrabble.getScore("g"));
     }
 
     @Test
     public void scrabbleScore_returnsAscrabbleScoreForASpace_0() {
-        Scrabble testScrabble = new Scrabble();
         Integer score = 0;
-        assertEquals(score, testScrabble.getScore(" "));
+        assertEquals(score, Scrabble.getScore(" "));
     }
 
     @Test
     public void scrabbleScore_returnsScoreSumForMutipleLetters_5() {
-        Scrabble testScrabble = new Scrabble();
         Integer score = 5;
-        assertEquals(score, testScrabble.getScore("dog"));
+        assertEquals(score, Scrabble.getScore("dog"));
     }
 
     @Test
     public void scrabbleScore_ReturnsCorrectScoreRegardlessOfCase_5() {
-        Scrabble testScrabble = new Scrabble();
         Integer score = 5;
-        assertEquals(score, testScrabble.getScore("dOg"));
+        assertEquals(score, Scrabble.getScore("dOg"));
     }
 }
